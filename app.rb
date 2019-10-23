0398ae074a7ea7ac81ae2404892e14e8b981a831
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-23 18:29:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,9 +14,9 @@ module FormsLab
     post '/pirates' do 
       @pirate = Pirate.new(params[:pirate])
       
-      # params[:pirate][:ships].each do |details|
-      #   Ship.new(details)
-      # end 
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end 
       
       @ships = Ship.all 
       
