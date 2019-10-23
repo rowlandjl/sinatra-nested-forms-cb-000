3501c4e2df9ec08e950a52cc3aa190543425bf95
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-23 18:29:43 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+  
+  @@all = []
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    @all << self 
+  end 
+  
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+  
+  @@all = []
+  
+  def initialize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.clear 
+    @@all.clear 
+  end 
+  
 end
